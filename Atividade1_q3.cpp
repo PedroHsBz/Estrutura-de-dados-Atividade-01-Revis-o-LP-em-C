cbe4e982898172063395e1d01b04e004a46e8221
Pedro Bazilio <pedrohenriquebazilio8@gmail.com>
2026-02-20 10:44:53 -0300

    Refactor password validation logic and improve checks

--- Atividade1_q3.cpp
+++ Atividade1_q3.cpp
@@ -1,23 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #define TAM 50 
-
 int main() {
     char senha[TAM];
     int c = 0, t = 0; 
     scanf("%[^\n]%c", senha); 
-
-    for (int i = 0; i < TAM; i++) {
-        if (senha[i] == '\0') {
-            break; 
-        }
-
+    for (int i = 0; i < strlen(senha); i++){
         if (senha[i] == '@' || senha[i] == '#' || senha[i] == '$' || 
             senha[i] == '%' || senha[i] == '&' || senha[i] == '*') {
             c++;
-        }
-        t++; 
+        } 
     }
-    if (t < 8) {
+    if (strlen(senha) < 8) {
         printf("Invalida! A senha deve ter pelo menos 8 caracteres.\n");
     } else if (c == 0) {
         printf("Invalida! A senha deve conter pelo menos um caractere especial (@, #, $, etc).\n");
